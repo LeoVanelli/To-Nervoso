7ff25cdec107ef6771b421433b4431f621b63db9
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-14 22:20:18 -0300

    Adiciona atividade ToNervoso

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace To_Nervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}, o que você pensa sobre mim? Você é muito sem graça! ",Environment.UserName);
+
+            Console.Write("Digite a seguir sua resposta: ");
+            string frase = Console.ReadLine();
+
+            Console.WriteLine("- E sabe o que eu penso de você ?");
+            Console.WriteLine("-{0}... NÃO, PERA.\n-Tô nervoso",frase.Remove(10));
+        }
+    }
+}
